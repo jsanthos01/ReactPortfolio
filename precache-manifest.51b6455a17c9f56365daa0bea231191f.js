a3c5c7b3f3110e2e3637da2c570316664bbe44bd
jsanthos01 <chinu.s2001@gmail.com>
2020-04-14 17:08:54 -0400

    Updates

---
+++ precache-manifest.51b6455a17c9f56365daa0bea231191f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70836925f40e02bd64fc643b6aed9f9",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "044bd8770cd3b972ce2b",
+    "url": "/ReactPortfolio/static/css/main.539a7da9.chunk.css"
+  },
+  {
+    "revision": "bba50a82c1d93d00192a",
+    "url": "/ReactPortfolio/static/js/2.58e51951.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.58e51951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044bd8770cd3b972ce2b",
+    "url": "/ReactPortfolio/static/js/main.24a253b0.chunk.js"
+  },
+  {
+    "revision": "906f8115bf5b3a63ebd5",
+    "url": "/ReactPortfolio/static/js/runtime-main.408cd239.js"
+  }
+]);
